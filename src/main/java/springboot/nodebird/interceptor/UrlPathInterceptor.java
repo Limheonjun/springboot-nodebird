53026f98b3dbb3d28bd8ba5fa5f03b54331add28
limheonjun <jumong40@naver.com>
2020-10-04 11:26:33 +0900

    URL 경로 표시 인터셉터 추가

---
+++ src/main/java/springboot/nodebird/interceptor/UrlPathInterceptor.java
@@ -0,0 +1,19 @@
+package springboot.nodebird.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UrlPathInterceptor extends HandlerInterceptorAdapter {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("URL PATH : " + request.getRequestURI());
+        return true;
+    }
+}
