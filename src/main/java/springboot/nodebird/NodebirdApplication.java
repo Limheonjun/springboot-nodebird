7334a782b9931e2dd41b74f1af229b489354e933
limheonjun <jumong40@naver.com>
2020-10-04 11:25:49 +0900

    initial commit

---
+++ src/main/java/springboot/nodebird/NodebirdApplication.java
@@ -0,0 +1,13 @@
+package springboot.nodebird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodebirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodebirdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/nodebird/NodebirdApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.nodebird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodebirdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
