ad6b35c628fff7bf505a5257fc58ca1b1b09019a
limheonjun <jumong40@naver.com>
2020-10-05 20:13:11 +0900

    CORS 허용

--- src/main/java/springboot/nodebird/config/WebMvcConfingurer.java
+++ src/main/java/springboot/nodebird/config/WebMvcConfingurer.java
@@ -1,6 +1,7 @@
 package springboot.nodebird.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springboot.nodebird.interceptor.UrlPathInterceptor;
@@ -11,5 +12,11 @@ public class WebMvcConfingurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UrlPathInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
