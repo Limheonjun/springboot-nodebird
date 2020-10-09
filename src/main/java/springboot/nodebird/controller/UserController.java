901945e443f3d8c6d9a236493690e2d2e395f678
limheonjun <jumong40@naver.com>
2020-10-09 20:58:50 +0900

    security의 인증사용방법 미숙 => 하드코딩으로 로그인 구현

--- src/main/java/springboot/nodebird/controller/UserController.java
+++ src/main/java/springboot/nodebird/controller/UserController.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.springframework.security.crypto.bcrypt.BCrypt.*;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -28,7 +30,7 @@ public class UserController {
             return "이미 사용중인 아이디 입니다";
         }
 
-        String passwordHashed = BCrypt.hashpw(m.get("password"), BCrypt.gensalt());
+        String passwordHashed = hashpw(m.get("password"), gensalt());
         Users users = Users.builder()
                 .email(m.get("email"))
                 .nickname(m.get("nickname"))
@@ -36,6 +38,24 @@ public class UserController {
                 .build();
         System.out.println("passwordHashed : " + passwordHashed);
         userRepository.save(users);
+        return "ok";
+    }
+
+    @PostMapping("/login")
+    public String logIn(@RequestBody Map<String, String> m, HttpServletResponse response) {
+        Users findUsers = userRepository.findByEmail(m.get("email"));
+        if(findUsers == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return "존재하지 않는 아이디 입니다";
+        }
+
+        String passwordHashed = hashpw(m.get("password"), gensalt());
+        if(!checkpw(m.get("password"), findUsers.getPassword())){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return "비밀번호가 일치하지 않습니다";
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
         return "ok";
     }
 }
