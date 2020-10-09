3442c0cf94bacef256bb33fe0822cbab61eb55ed
limheonjun <jumong40@naver.com>
2020-10-09 21:32:32 +0900

    Users fetch join 메소드 추가

--- src/main/java/springboot/nodebird/repository/UserRepository.java
+++ src/main/java/springboot/nodebird/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package springboot.nodebird.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import springboot.nodebird.entity.Users;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     Users findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"postsList", "followings", "followers"})
+    Users findFetchJoinByEmail(String email);
 }
