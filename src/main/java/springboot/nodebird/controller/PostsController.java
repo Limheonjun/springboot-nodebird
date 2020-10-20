4fc653ed6258f1c3215e1dc2771a5b2c5b557a33
limheonjun <jumong40@naver.com>
2020-10-20 20:09:03 +0900

    posts컨트롤러 추가

---
+++ src/main/java/springboot/nodebird/controller/PostsController.java
@@ -0,0 +1,28 @@
+package springboot.nodebird.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+import springboot.nodebird.dto.PostDTO;
+import springboot.nodebird.entity.Posts;
+import springboot.nodebird.repository.PostRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @PostMapping("/")
+    public Object getPosts() {
+        PageRequest pageRequest = PageRequest.of(0,  10);
+        Page<Posts> page = postRepository.findAll(pageRequest);
+        List<PostDTO> results = page.stream().map(PostDTO::new).collect(Collectors.toList());
+        return results;
+    }
+}
